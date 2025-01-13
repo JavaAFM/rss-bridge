8631ba9926f74ed9ad3657f545ef3f4c8b67172c
Alisher Khairullin <alisherkhairullin463@gmail.com>
2025-01-13 10:43:30 +0500

    Initial structure

---
+++ src/main/java/org/AFM/rssbridge/RssBridgeApplication.java
@@ -0,0 +1,13 @@
+package org.AFM.rssbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RssBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/AFM/rssbridge/RssBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.AFM.rssbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
