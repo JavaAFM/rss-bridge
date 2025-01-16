93e28707375e082b64ed8c62f7635520d44c7fbd
R0onen <erni100105@gmail.com>
2025-01-16 11:30:01 +0500

    feat: implement equals and hashCode for Comment; improve comment fetching logic for uniqueness

--- src/main/java/org/AFM/rssbridge/model/Comment.java
+++ src/main/java/org/AFM/rssbridge/model/Comment.java
@@ -1,6 +1,7 @@
 package org.AFM.rssbridge.model;
 
 import lombok.*;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -19,4 +20,17 @@ public class Comment {
 
     private String content;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(author, comment.author) && 
+               Objects.equals(content, comment.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, content);
+    }
 }
