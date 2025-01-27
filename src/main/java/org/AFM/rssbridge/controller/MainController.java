16730ac1fc0a8e2ece0b0af360df94d09cf87c90
R0onen <erni100105@gmail.com>
2025-01-27 12:28:56 +0500

    feat: added updating db every 2 minutes but not fully working

--- src/main/java/org/AFM/rssbridge/controller/MainController.java
+++ src/main/java/org/AFM/rssbridge/controller/MainController.java
@@ -7,6 +7,7 @@ import org.AFM.rssbridge.model.News;
 import org.AFM.rssbridge.service.scrapper.*;
 import org.jsoup.select.Elements;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,33 +23,73 @@ public class MainController {
     private final OrdaService ordaService;
     private final ZakonService zakonService;
 
+    @Scheduled(fixedRate = 120000)
     @GetMapping("/tengri")
     public ResponseEntity<List<News>> getTengriNews() throws NotFoundException {
         Elements elements = tengriService.allNewsElements();
+        //        List<News> parsedNews = tengriService.toNews(elements);
+//            for (News news : parsedNews) {
+//                if () {
+//                    System.out.println("New article saved: " + news.getTitle());
+//                } else {
+//                    System.out.println("Article already exists: " + news.getTitle());
+//                }
         return ResponseEntity.ok(tengriService.toNews(elements));
     }
 
+    @Scheduled(fixedRate = 120000)
     @GetMapping("/kaztag")
     public ResponseEntity<List<News>> getKaztagNews() throws NotFoundException {
         Elements elements = kaztagService.allNewsElements();
+        //        List<News> parsedNews = kaztagService.toNews(elements);
+//            for (News news : parsedNews) {
+//                if () {
+//                    System.out.println("New article saved: " + news.getTitle());
+//                } else {
+//                    System.out.println("Article already exists: " + news.getTitle());
+//                }
         return ResponseEntity.ok(kaztagService.toNews(elements));
     }
 
+    @Scheduled(fixedRate = 120000)
     @GetMapping("/azattyq")
     public ResponseEntity<List<News>> getAzattyqNews() throws NotFoundException {
         Elements elements = azattyqService.allNewsElements();
+        //        List<News> parsedNews = azattyqService.toNews(elements);
+//            for (News news : parsedNews) {
+//                if () {
+//                    System.out.println("New article saved: " + news.getTitle());
+//                } else {
+//                    System.out.println("Article already exists: " + news.getTitle());
+//                }
         return ResponseEntity.ok(azattyqService.toNews(elements));
     }
 
+    @Scheduled(fixedRate = 120000)
     @GetMapping("/zakon")
     public ResponseEntity<List<News>> getZakonNews() throws NotFoundException {
         Elements elements = zakonService.allNewsElements();
+        //        List<News> parsedNews = zakonService.toNews(elements);
+//            for (News news : parsedNews) {
+//                if () {
+//                    System.out.println("New article saved: " + news.getTitle());
+//                } else {
+//                    System.out.println("Article already exists: " + news.getTitle());
+//                }
         return ResponseEntity.ok(zakonService.toNews(elements));
     }
 
+    @Scheduled(fixedRate = 120000)
     @GetMapping("/orda")
     public ResponseEntity<List<News>> getOrdaNews() throws NotFoundException {
         Elements elements = ordaService.allNewsElements();
+        //        List<News> parsedNews = ordaService.toNews(elements);
+//            for (News news : parsedNews) {
+//                if () {
+//                    System.out.println("New article saved: " + news.getTitle());
+//                } else {
+//                    System.out.println("Article already exists: " + news.getTitle());
+//                }
         return ResponseEntity.ok(ordaService.toNews(elements));
     }
 
